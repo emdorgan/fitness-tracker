5c97605f87f04cb4021aa8993e17f621c4d33da1
Emily Dorgan <wmdorgan@gmail.com>
2021-11-11 19:57:49 -0800

    set up the server.js dependencies and the middlware

---
+++ server.js
@@ -0,0 +1,26 @@
+//all dependencies
+
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// tells the app to use the public directory for client-side html/css/js
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
