2ba2d17f8ec3b2a29198ef52e0e94a8e7bba4dbc
Emily Dorgan <wmdorgan@gmail.com>
2021-11-12 19:23:03 -0800

    set up the create workout route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
 const {Workout, Exercise} = require("../models");
 
+// GET route that fetches the last workout
 
+
+// POST route that creates a new workout
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+      });
+})
 
 module.exports = router;
