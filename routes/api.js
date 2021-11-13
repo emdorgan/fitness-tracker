1d45896b5177eb260198d95f5db7ed20ac4aaab4
Emily Dorgan <wmdorgan@gmail.com>
2021-11-12 17:59:44 -0800

    removed outdated dependencies, added to do list to readme, changed how routes and models are structured in file structure

---
+++ routes/api.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Workout = require("../models/workoutModel.js");
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //all dependencies
 
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
 const path = require("path");
 const mongoose = require("mongoose");
@@ -9,7 +8,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const Workout = require('./workoutModel.js')
+const Workout = require('./models/workoutModel.js')
 
 //Middleware
 app.use(logger("dev"));
@@ -20,8 +19,12 @@ app.use(express.json());
 // tells the app to use the public directory for client-side html/css/js
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
